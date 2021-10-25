dcdfe5ca1fa3eba7dd60a1d4274318100a31599b
NTsoi <czoj.n95@mail.ru>
2021-10-25 21:45:27 +0300

    adds indexOf, LastIndexOf, removeIf interfaces with pattern and predicate

--- list-interface/src/telran/util/List.java
+++ list-interface/src/telran/util/List.java
@@ -1,5 +1,7 @@
 package telran.util;
 
+import java.util.function.Predicate;
+
 public interface List<T> {
 /**
  * adds a given element at end of list
@@ -34,5 +36,52 @@ public interface List<T> {
 	 * @return reference to being removed element or null if the index is incorrect (index < 0 || index >= size)
 	 */
 	T remove(int index);
-
+	
+	/**
+	 * check existence of an object
+	 * @param pattern
+	 * @return true if there is at least one object equaled to a given pattern, otherwise- false
+	 */
+	boolean contains(T pattern);
+	
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the first occurrence of an object equaled to a given pattern, or -1 if no such object
+	 */
+	int indexOf(T pattern);
+	
+	/**
+	 * 
+	 * @param pattern
+	 * @return index of the last occurrence of an object equaled to a given pattern, or -1 if no such object
+	 */
+	int LastIndexOf(T pattern);
+	
+	/**
+	 * 
+	 * @param predicate
+	 * @return true in the case the list contains at least one object matching a condition of a given predicate
+	 */
+	boolean contains(Predicate<T> predicate);
+	/**
+	 * 
+	 * @param predicate
+	 * @return index of the first occurrence of an object matching a given predicate, or -1 if no such object
+	 */
+	int indexOf(Predicate<T> predicate);
+	
+	/**
+	 * 
+	 * @param predicate
+	 * @return index of the last occurrence of an object matching a given predicate, or -1 if no such object
+	 */
+	int LastIndexOf(Predicate<T> predicate);
+	
+	/**
+	 * removes all objects matching a given predicate
+	 * @param predicate
+	 * @return true if at least one object has been removed, otherwise - false 
+	 */
+	boolean removeIf(Predicate<T> predicate);
 }
